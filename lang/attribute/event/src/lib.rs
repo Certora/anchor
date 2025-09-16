34cb5a42f5cfe754c4d38c3d1d07aace9258e8ab
Arie Gurfinkel <arie@certora.com>
2025-09-16 15:19:02 -0400

    certora: silence emit! macro

--- lang/attribute/event/src/lib.rs
+++ lang/attribute/event/src/lib.rs
@@ -96,7 +96,7 @@ pub fn derive_event(_item: proc_macro::TokenStream) -> proc_macro::TokenStream {
 #[proc_macro]
 pub fn emit(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     // CERTORA: For verification this is a non-op
-    proc_macro::TokenStream::new()
+    "()".parse().unwrap()	
 }
 
 /*
